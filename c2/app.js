1f8583d5fcd1180988e8bf2881f26fd62eceb13f
Dwight-jz <102237287+Dwight-jz@users.noreply.github.com>
2022-03-24 11:58:22 -0600

    Add files via upload

--- c2/app.js
+++ c2/app.js
@@ -1,89 +1,116 @@
-// Eventos DOM (Document Objet Model)
 document
-.getElementById("Producto-formulario")
-.addEventListener("submit", function(evento){
-// evaluar el comportamiento del formulario
+.getElementById("producto-formulario")
+.addEventListener("submit", function(evento)
+{
+    evento.preventDefault();
 
-evento.preventDefault(); // "prevenDefault" para guardar sin refrescar en la pagina
+    //Obtener los valores del formulario
+    const nombre= document.getElementById("nombre").value,
+          precio= document.getElementById("precio").value,
+          año= document.getElementById("año").value;
 
-// Obtener los valores del formulario
-const nombre = document.getElementById("nombre").value,
-     precio = document.getElementById("precio").value,
-     año = document.getElementById("año").value;
+    //Crear un nuevo objeto (Es para ingresar productos)
+    const producto = new Producto (nombre, precio, año);
+    
+    //Crear un nuevo usuario de interfaz
+    const ui = new UI();
 
-     //crear un nuevo objeto "producto"
-     const producto = new Product (nombre,precio,año);
 
-     //Crear un nuevo usuario de interfaz
-     const ui = new UI();
+    //Input para la validación de los datos
+    if (nombre === " " || precio === " " || año === " " )
+    {
+        ui.showMessage("Por favor insertar datos");  
+    }
 
-     //Input de validacion de usuario
-     if(nombre === "" || precio === "" || año === ""){
-         ui.showMessage("Por favor insertar datos");
-     } 
-
-     //Guardar producto
-     ui.addProducto(producto);
-     ui.showMessage("Producto agregado");
-     ui.resetForm();
+    //Guardar datos
+    ui.addProduct(producto),
+    ui.showMessage("Producto Agregado"); 
+    ui.resetFrom();
+  
 });
 
-document.getElementById("producto-lista").addEventListener("click", (e) =>{
-    const ui = new UI();
-    ui.deleteProducto(e.target);
-    e.preventDefault();
-})
-class Product{
-    constructor(nombre,precio,año){
+document.getElementById("producto-lista").addEventListener("click", (evento) =>{
+     const ui = new UI();
+     ui.deleteProducto(evento.target);
+     e.preventDefault();
+     window.alert("Producto Eliminado");
+});
+
+
+//Clase Producto
+
+class Producto
+{
+    constructor(nombre, precio, año)
+    {
         this.nombre = nombre;
-        this.precio =precio;
-        this.año =año;
+        this.precio = precio;
+        this.año = año;
     }
 }
-class UI{
-    addProducto(producto){
-        const producLista = document.getElementById("producto-lista");
-        const elemento = document.createElement("div");
-        elemento.innerHTML = `
-        <div class="tarjeta textro margen4">
-        <div class="tarjeta-body">
-            <strong>producto</strong> : ${producto.nombre} -
-                <strong>precio</strong>: ${producto.precio} -
-                <strong>año</strong>: ${producto.año} -
-                <a href="#" class="btn btn-error" name="eliminar">Eliminar</a>
-        </div>
-    </div>
-    `;
- 
-    producLista.appendChild(Element); //appendchild elemento fijo
-    //resetear valores
-} 
-    resetForm(){
-        document.getElementById("Producto-formulario").reset();       
-    }
 
-    deleteProducto(elemento){
-        if (element.nombre==="eliminar" ){
-            element.parentElement.parentElement.remove();
-            this.showMessage("El producto se a eliminado")
-        }
-    }
-    showMessage(mensaje,cssClass){
-        const div = document.createElement("div");
-        div.className = `alert alert-${cssClass} `;
-        div.appendChild(document.createTextNode(mensaje));
-        //Mostrar en el DOM
-        const contenido = document.querySelector(".container");
-        const app = document.querySelector("#App");
+
+//Clase Usuario interfaz
+
+class UI
+{
+  addProduct(producto)
+  {
+      const productoLista = document.getElementById("producto-lista");
+      const elemento = document.createElement("div");
+      elemento.innerHTML=`
+      <div class="tarjeta texto margen4" id="info" style="margin-left: 40%;">
+            <div class="tarjeta-body">
+            <br>
+            <strong> PRODUCTO </strong> :  ${producto.nombre} -
+            <strong> PRECIO </strong> :  ${producto.precio} -
+            <strong> AÑO </strong> :  ${producto.año} - <br><br>
+            <a href="" class="btn btn-lg btn-primary" id="btneli" name="eliminar"style="margin-left: -02%; margin-bottom: 2%; height: 15%; width: 80%; font-size: 90%;" > Eliminar  </a> 
+            </div>
+            </div>
+            `;
+            window.alert("Producto agregado");
+
+productoLista.appendChild(elemento); //appendChild es un elemento fijo
+}
+//resetear datos de formulario
+resetFrom()
+{
+    
+document.getElementById("producto-formulario").reset();
+}
+
+deleteProducto(elemento)
+{
+    
+    if(elemento.nombre === "eliminar")
+    {
+        this.showMessage("El Producto se ha Eliminado");
+        elemento.parentElement.parentElement.remove();
         
-        // insert message in the interfaz of user
-        container.insertBefore(div, app);
-
-        // Remover el mensaje luego de 3 segundos
-
-        setTimeout(function(){
-            document.querySelector(".alert").remove();
-        }, 3000);
     }
+}
 
-    }
+showMessage(mensaje, cssClass)
+{
+  const div = document.createElement("div");
+  div.className = `alert alert-${cssClass}`;
+  div.appendChild(document.createTextNode(mensaje));
+
+
+
+//Mostrar en el DOM
+const contenido = document.querySelector(".container")
+const app = document.querySelector("#App")
+    //Insertar mensaje en el interfaz usuario
+    container.insertBefore(div, app);
+
+    //Remover el mensaje luego de tres segundos
+
+    setTimeout(function ()
+    {
+        document.querySelector(".alert").remove();
+    }, 3000);
+
+}
+}
